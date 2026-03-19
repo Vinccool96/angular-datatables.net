6d6199eb77da91f56ba40c682c7599bf652f940a
Vinccool96 <vinccool96@gmail.com>
2026-03-19 12:25:38 -0400

    Fix lint

--- demo/src/app/advanced/rerender/rerender.component.spec.ts
+++ demo/src/app/advanced/rerender/rerender.component.spec.ts
@@ -35,9 +35,7 @@ describe('RerenderComponent', () => {
     const rerenderSpy = spyOn(component, 'rerender');
 
     const triggerBtns = [...spectator.queryAll<HTMLButtonElement>('button')];
-    const triggerButton = triggerBtns.find(
-      (element) => element.textContent?.includes('Rerender') ?? false,
-    ) as HTMLButtonElement;
+    const triggerButton = triggerBtns.find((element) => element.textContent.includes('Rerender')) as HTMLButtonElement;
 
     triggerButton.click();
     triggerButton.dispatchEvent(new Event('click'));
