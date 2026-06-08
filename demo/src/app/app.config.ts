4060550c4e0f58839984a7811dd1c881235d53ab
Vinccool96 <vinccool96@gmail.com>
2026-06-08 09:33:14 -0400

    Updated dependencies

--- demo/src/app/app.config.ts
+++ demo/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withXhr } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection, SecurityContext } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideMarkdown, SANITIZE } from 'ngx-markdown';
@@ -9,7 +9,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withXhr()),
     provideMarkdown({
       sanitize: {
         provide: SANITIZE,
