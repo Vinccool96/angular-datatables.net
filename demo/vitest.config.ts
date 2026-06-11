382ad0df1f7b0d293b77bf0baba9de2c89553001
Vinccool96 <vinccool96@gmail.com>
2026-06-11 10:41:58 -0400

    Switch to firefox

--- demo/vitest.config.ts
+++ demo/vitest.config.ts
@@ -1,21 +1,7 @@
-import { webdriverio } from '@vitest/browser-webdriverio';
-import path from 'node:path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    browser: {
-      enabled: true,
-      instances: [{ browser: 'firefox', headless: true }],
-      provider: webdriverio({
-        capabilities: {
-          'browserName': 'firefox',
-          'wdio:geckodriverOptions': {
-            binary: path.resolve(process.cwd(), 'drivers/geckodriver.exe'),
-          },
-        },
-      }),
-    },
     setupFiles: './demo/setup-test.ts',
   },
 });
